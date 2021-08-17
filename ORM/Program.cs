63d82564f5c4541348053dc52e95d37d3637f531
DW Rodrigues <denys.w.r@hotmail.com>
2021-08-16 21:50:44 -0300

    Adicionar arquivos de projeto.

---
+++ ORM/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ORMContext ctx = new ORMContext())
+            {
+                Aluno a = new Aluno()
+                {
+                    Nome = "Christian",
+                    Email = "chris@gmail.com"
+                };
+
+                ctx.Alunos.Add(a);
+                ctx.SaveChanges();
+
+                Console.WriteLine("Sucesso!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
